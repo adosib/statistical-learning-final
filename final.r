5ae6f141e8e21ccac935a7150fc853c567317d06
Ado Sibalo <asibalo@mail.missouri.edu>
2018-05-04 11:47:44 -0500

    dropped variable District, which resulted in a .2% increase in model test accuracy.

--- final.r
+++ final.r
@@ -4,14 +4,15 @@ df = read.xlsx('Project Data.xlsx', sheet=1)
 
 #drop variables consisting of character sets and variables not useful for model
 drops <- c("Cause.of.Death","Date.of.Death","Year","Surname",
-           "Given.Names","Notes","Place.of.Birth","Place.of.Death","Place.of.Internment")
+           "Given.Names","Notes","Place.of.Birth","Place.of.Death","Place.of.Internment",
+           "District")
 df = df[ , !(names(df) %in% drops)]
 
 
 #convert to factors the variables that are categorical in nature
-df[c('Sex','monthYear','Abbrev..Cause.of.Death','Religion','District','Region')] <-
+df[c('Sex','monthYear','Abbrev..Cause.of.Death','Religion','Region')] <-
   list(as.factor(df$Sex),as.factor(df$monthYear),as.factor(df$Abbrev..Cause.of.Death),
-       as.factor(df$Religion),as.factor(df$District),as.factor(df$Region))
+       as.factor(df$Religion),as.factor(df$Region))
 
 set.seed(1)
 train.set = sample(1:nrow(df),.60*nrow(df),
@@ -28,6 +29,6 @@ yhat.rf=predict(deaths.rf,newdata=test)
 table(observed = test$Sex, predicted = yhat.rf)
 importance(deaths.rf)
 
-(388+536)/(388+536+319+475)
+(414+479)/(414+479+376+449)
 
 
