116297049e9fee236461956d7961af2b11af6d43
Ado Sibalo <asibalo@mail.missouri.edu>
2018-05-04 11:18:27 -0500

    Changes to variables being read in

--- final.r
+++ final.r
@@ -3,8 +3,8 @@ library(openxlsx)
 df = read.xlsx('Project Data.xlsx', sheet=1)
 
 #drop variables consisting of character sets and variables not useful for model
-drops <- c("Cause.of.Death","monthYear","Surname","Given.Names","Notes",
-           "Place.of.Birth","Place.of.Death","Place.of.Internment")
+drops <- c("Cause.of.Death","Date.of.Death","Year","monthYear","Surname",
+           "Given.Names","Notes","Place.of.Birth","Place.of.Death","Place.of.Internment")
 df = df[ , !(names(df) %in% drops)]
 
 #convert to factors the variables that are categorical in nature
@@ -13,7 +13,7 @@ df[c('Sex','Abbrev..Cause.of.Death','Religion','District','Region')] <-
        as.factor(df$Religion),as.factor(df$District),as.factor(df$Region))
 
 set.seed(1)
-train.set = sample(1:nrow(df),.65*nrow(df),
+train.set = sample(1:nrow(df),.60*nrow(df),
                    replace=FALSE)
 train = df[train.set,]
 test = df[-train.set,]
