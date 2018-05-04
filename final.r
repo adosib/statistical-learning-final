7028b8e7f310e94a30329574882a62ae41c2330c
Ado Sibalo <asibalo@mail.missouri.edu>
2018-05-04 12:08:17 -0500

    readded variables District and Region

--- final.r
+++ final.r
@@ -4,15 +4,14 @@ df = read.xlsx('Project Data.xlsx', sheet=1)
 
 #drop variables consisting of character sets and variables not useful for model
 drops <- c("Cause.of.Death","Date.of.Death","Year","Surname",
-           "Given.Names","Notes","Place.of.Birth","Place.of.Death","Place.of.Internment",
-           "District")
+           "Given.Names","Notes","Place.of.Birth","Place.of.Death","Place.of.Internment")
 df = df[ , !(names(df) %in% drops)]
 
 
 #convert to factors the variables that are categorical in nature
-df[c('Sex','monthYear','Abbrev..Cause.of.Death','Religion','Region')] <-
+df[c('Sex','monthYear','Abbrev..Cause.of.Death','Religion','District','Region')] <-
   list(as.factor(df$Sex),as.factor(df$monthYear),as.factor(df$Abbrev..Cause.of.Death),
-       as.factor(df$Religion),as.factor(df$Region))
+       as.factor(df$Religion),as.factor(df$District),as.factor(df$Region))
 
 set.seed(1)
 train.set = sample(1:nrow(df),.60*nrow(df),
@@ -29,6 +28,6 @@ yhat.rf=predict(deaths.rf,newdata=test)
 table(observed = test$Sex, predicted = yhat.rf)
 importance(deaths.rf)
 
-(414+479)/(414+479+376+449)
+(388+536)/(388+536+319+475)
 
 
