d66215c84c33b186069ebdd0b3a47bbb8582ab02
taylorpaskoff <38954088+taylorpaskoff@users.noreply.github.com>
2018-05-04 12:14:12 -0500

    added notes for pipe

---
+++ gam.R
@@ -0,0 +1,39 @@
+# FINAL PROJECT
+# GAMs
+# taylor
+
+
+# load data
+library(readr)
+Project_Data <- read_csv("~/School/Applied Statistical Models/Project Data.csv")
+projectdata <- Project_Data
+
+library(gam)
+library(rgr)
+library(dplyr)
+
+projectdata <- projectdata[-c(1, 3, 4, 6, 7, 12:14, 17:25)]
+colnames(projectdata) <- c("MY", "CoD", "Age", "Sex", "SexRatio", "Religion", "District", "Region")
+
+# changed sex to M = 1 and F = 0
+attach(projectdata)
+projectdata$Sex[which(projectdata$Sex == "M")] <- 1 
+projectdata$Sex[which(projectdata$Sex == "F")] <- 0
+
+# %>% is the pipe function; tell it to only give you things that are not NA values for that vector in the data frame. 
+# need to load "dplyr" function
+projectdata <- projectdata %>% filter(!is.na(Age))
+projectdata <- projectdata %>% filter(!is.na(Sex))
+projectdata <- projectdata %>% filter(!is.na('Death date'))
+projectdata <- projectdata %>% filter(!is.na(Religion))
+
+projectdata[c('MY','CoD','Religion', 'District', 'Region')] <-
+  list(as.factor(projectdata$MY),as.factor(projectdata$CoD),as.factor(projectdata$Religion), 
+       as.factor(projectdata$District),as.factor(projectdata$Region))
+
+set.seed(1)
+train.set <- sample(1:nrow(projectdata), 0.9*nrow(projectdata), replace = FALSE)
+train <- projectdata[train.set,]
+test <- projectdata[-train.set,]
+
+gam(Sex ~., family = "binomial", data = train)
