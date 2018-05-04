27a4c1062c51c24244f825b30600a5f5851cce5a
Ado Sibalo <asibalo@mail.missouri.edu>
2018-05-04 10:58:25 -0500

    Preliminary model completed

--- final.r
+++ final.r
@@ -1,12 +1,31 @@
 install.packages('openxlsx')
 library(openxlsx)
 df = read.xlsx('Project Data.xlsx', sheet=1)
-drops <- c("Cause.of.Death","Month.&.Year","Surname","Given.Names")
+
+#drop variables consisting of character sets and variables not useful for model
+drops <- c("Cause.of.Death","monthYear","Surname","Given.Names","Notes",
+           "Place.of.Birth","Place.of.Death","Place.of.Internment")
 df = df[ , !(names(df) %in% drops)]
+
+#convert to factors the variables that are categorical in nature
+df[c('Sex','Abbrev..Cause.of.Death','Religion','District','Region')] <-
+  list(as.factor(df$Sex),as.factor(df$Abbrev..Cause.of.Death),
+       as.factor(df$Religion),as.factor(df$District),as.factor(df$Region))
 
 set.seed(1)
 train.set = sample(1:nrow(df),.65*nrow(df),
                    replace=FALSE)
 train = df[train.set,]
 test = df[-train.set,]
+train_index=c(as.numeric(rownames(train)))
+
+library(tree)
+library(randomForest)
+
+deaths.rf = randomForest(Sex~.,data=train,na.action='na.omit',importance=TRUE)
+yhat.rf=predict(deaths.rf,newdata=test)
+table(observed = test$Sex, predicted = yhat.rf)
+importance(deaths.rf)
+
+
 
